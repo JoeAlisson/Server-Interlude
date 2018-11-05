669e36dc7cd799c406ff83ea6e1ee94811645b9f
Alisson Oliveira <joe.alisson@gmail.com>
2018-11-04 22:12:38 -0300

    Refactoring NpcStat

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/model/actor/stat/PlayableStat.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/model/actor/stat/PlayableStat.java
@@ -1,59 +1,30 @@
-/* This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
- *
- * http://www.gnu.org/copyleft/gpl.html
- */
 package org.l2j.gameserver.model.actor.stat;
 
 import org.l2j.gameserver.model.actor.instance.L2PlayableInstance;
 import org.l2j.gameserver.model.base.Experience;
 
+public class PlayableStat extends CharStat {
 
-public class PlayableStat extends CharStat
-{
-	// =========================================================
-	// Data Field
-	
-	// =========================================================
-	// Constructor
 	public PlayableStat(L2PlayableInstance activeChar)
 	{
 		super(activeChar);
 	}
 	
-	// =========================================================
-	// Method - Public
-	public boolean addExp(long value)
-	{
-		if (((getExp() + value) < 0) || (getExp() == (getExpForLevel(Experience.MAX_LEVEL) - 1)))
-		{
+
+	public boolean addExp(long value) {
+		if (((getExp() + value) < 0) || (getExp() == (getExpForLevel(Experience.MAX_LEVEL) - 1))) {
 			return true;
 		}
 		
-		if ((getExp() + value) >= getExpForLevel(Experience.MAX_LEVEL))
-		{
+		if ((getExp() + value) >= getExpForLevel(Experience.MAX_LEVEL)) {
 			value = getExpForLevel(Experience.MAX_LEVEL) - 1 - getExp();
 		}
 		
 		setExp(getExp() + value);
 		
 		byte level = 0;
-		for (level = 1; level <= Experience.MAX_LEVEL; level++)
-		{
-			if (getExp() >= getExpForLevel(level))
-			{
+		for (level = 1; level <= Experience.MAX_LEVEL; level++) {
+			if (getExp() >= getExpForLevel(level)) {
 				continue;
 			}
 			level--;
